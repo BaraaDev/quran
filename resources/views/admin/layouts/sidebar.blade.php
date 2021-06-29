fab5936b12e2f2e00aedd790053a711971fcd09e
baraaofficial <baraa_official@outlook.com>
2021-06-29 20:26:39 +0200

    update

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -22,7 +22,9 @@
         <ul class="nav nav-sidebar" data-nav-type="accordion">
 
             <!-- Main -->
-            <li class="nav-item-header"><div class="text-uppercase font-size-xs line-height-xs">قائمة التنقل والأعدادات</div> <i class="icon-menu" title="Main"></i></li>
+            <li class="nav-item-header"><div class="text-uppercase font-size-xs line-height-xs">قائمة التنقل والأعدادات</div> <i class="icon-menu" title="قائمة التنقل والأعدادات"></i></li>
+
+
             <li class="nav-item">
                 <a href="{{route('dashboard.home')}}" class="nav-link {{active()->route('dashboard.home')}}">
                     <i class="icon-home4"></i>
@@ -31,14 +33,9 @@
                     </span>
                 </a>
             </li>
-            <li class="nav-item nav-item-submenu">
-                <a href="{{route('tags.index')}}" class="nav-link {{active()->route('tags.*')}}"><i class="icon-thumbs-up2"></i> <span>العلامات</span></a>
-                <ul class="nav nav-group-sub" data-submenu-title="العلامات">
-                    <li class="nav-item"><a href="{{route('tags.index')}}" class="nav-link {{active()->route('tags.index')}}">جميع العلامات</a></li>
-                    <li class="nav-item"><a href="{{route('tags.create')}}" class="nav-link {{active()->route('tags.create')}}">إضافة علامة جديدة</a></li>
-                 </ul>
-            </li>
-            <li class="nav-item nav-item-submenu">
+
+
+            <li class="nav-item nav-item-submenu @if(active()->route('levels.*')) nav-item-expanded nav-item-open @else  @endif">
                 <a href="{{route('levels.index')}}" class="nav-link {{active()->route('levels.*')}}"><i class="icon-stats-bars4"></i> <span>المستويات</span></a>
                 <ul class="nav nav-group-sub" data-submenu-title="المستويات">
                     <li class="nav-item"><a href="{{route('levels.index')}}" class="nav-link {{active()->route('levels.index')}}">جميع المستويات</a></li>
@@ -46,14 +43,17 @@
                 </ul>
             </li>
 
-            <li class="nav-item nav-item-submenu">
+
+            <li class="nav-item nav-item-submenu @if(active()->route('articles.*')) nav-item-expanded nav-item-open @else  @endif">
                 <a href="{{route('articles.index')}}" class="nav-link {{active()->route('articles.*')}}"><i class="icon-file-text3"></i> <span>المقالات</span></a>
                 <ul class="nav nav-group-sub" data-submenu-title="المقالات">
                     <li class="nav-item"><a href="{{route('articles.index')}}" class="nav-link {{active()->route('articles.index')}}">جميع المقالات</a></li>
                     <li class="nav-item"><a href="{{route('articles.create')}}" class="nav-link {{active()->route('articles.create')}}">إنشاء مقال جديد</a></li>
                 </ul>
             </li>
-            <li class="nav-item nav-item-submenu">
+
+
+            <li class="nav-item nav-item-submenu @if(active()->route('categories.*')) nav-item-expanded nav-item-open @else  @endif">
                 <a href="{{route('categories.index')}}" class="nav-link {{active()->route('categories.*')}}"><i class="icon-stack"></i> <span>الأقسام</span></a>
                 <ul class="nav nav-group-sub" data-submenu-title="الأقسام">
                     <li class="nav-item"><a href="{{route('categories.index')}}" class="nav-link {{active()->route('categories.index')}}">جميع الأقسام</a></li>
@@ -61,7 +61,17 @@
                 </ul>
             </li>
 
-            <li class="nav-item nav-item-submenu">
+
+            <li class="nav-item nav-item-submenu @if(active()->route('tags.*')) nav-item-expanded nav-item-open @else  @endif">
+                <a href="{{route('tags.index')}}" class="nav-link {{active()->route('tags.*')}}"><i class="icon-thumbs-up2"></i> <span>العلامات</span></a>
+                <ul class="nav nav-group-sub" data-submenu-title="العلامات">
+                    <li class="nav-item"><a href="{{route('tags.index')}}" class="nav-link {{active()->route('tags.index')}}">جميع العلامات</a></li>
+                    <li class="nav-item"><a href="{{route('tags.create')}}" class="nav-link {{active()->route('tags.create')}}">إضافة علامة جديدة</a></li>
+                </ul>
+            </li>
+
+
+            <li class="nav-item nav-item-submenu @if(active()->route('appointments.*')) nav-item-expanded nav-item-open @else  @endif">
                 <a href="{{route('appointments.index')}}" class="nav-link {{active()->route('appointments.*')}}"><i class="icon-alarm"></i> <span>المواعيد</span></a>
                 <ul class="nav nav-group-sub" data-submenu-title="المواعيد">
                     <li class="nav-item"><a href="{{route('appointments.index')}}" class="nav-link {{active()->route('appointments.index')}}">جميع المواعيد</a></li>
@@ -69,7 +79,8 @@
                 </ul>
             </li>
 
-            <li class="nav-item nav-item-submenu">
+
+            <li class="nav-item nav-item-submenu @if(active()->route('events.*')) nav-item-expanded nav-item-open @else  @endif">
                 <a href="{{route('events.index')}}" class="nav-link {{active()->route('events.*')}}"><i class="icon-calendar3"></i> <span>الأحداث</span></a>
                 <ul class="nav nav-group-sub" data-submenu-title="الأحداث">
                     <li class="nav-item"><a href="{{route('events.index')}}" class="nav-link {{active()->route('events.index')}}">جميع الأحداث</a></li>
@@ -77,7 +88,8 @@
                 </ul>
             </li>
 
-            <li class="nav-item nav-item-submenu">
+
+            <li class="nav-item nav-item-submenu @if(active()->route('services.*')) nav-item-expanded nav-item-open @else  @endif">
                 <a href="{{route('services.index')}}" class="nav-link {{active()->route('services.*')}}"><i class="icon-wrench3"></i> <span>الخدمات</span></a>
                 <ul class="nav nav-group-sub" data-submenu-title="الخدمات">
                     <li class="nav-item"><a href="{{route('services.index')}}" class="nav-link {{active()->route('services.index')}}">جميع الخدمات</a></li>
@@ -85,7 +97,8 @@
                 </ul>
             </li>
 
-            <li class="nav-item nav-item-submenu">
+
+            <li class="nav-item nav-item-submenu @if(active()->route('testimonials.*')) nav-item-expanded nav-item-open @else  @endif">
                 <a href="{{route('testimonials.index')}}" class="nav-link {{active()->route('testimonials.*')}}"><i class="icon-quotes-left"></i> <span>الأراء</span></a>
                 <ul class="nav nav-group-sub" data-submenu-title="الأراء">
                     <li class="nav-item"><a href="{{route('testimonials.index')}}" class="nav-link {{active()->route('testimonials.index')}}">جميع الأراء</a></li>
@@ -93,14 +106,17 @@
                 </ul>
             </li>
 
-            <li class="nav-item nav-item-submenu">
+
+            <li class="nav-item nav-item-submenu @if(active()->route('users.*')) nav-item-expanded nav-item-open @else  @endif">
                 <a href="{{route('users.index')}}" class="nav-link {{active()->route('users.*')}}"><i class="icon-users"></i> <span>المستخدمين</span></a>
                 <ul class="nav nav-group-sub" data-submenu-title="المستخدمين">
                     <li class="nav-item"><a href="{{route('users.index')}}" class="nav-link {{active()->route('users.index')}}">جميع المستخدمين</a></li>
                     <li class="nav-item"><a href="{{route('users.create')}}" class="nav-link {{active()->route('users.create')}}">إنشاء مستخدم جديد</a></li>
                 </ul>
             </li>
-            <li class="nav-item nav-item-submenu">
+
+
+            <li class="nav-item nav-item-submenu @if(active()->route('mail.*')) nav-item-expanded nav-item-open @else  @endif">
                 <a href="{{route('mail.index')}}" class="nav-link {{active()->route('mail.*')}}"><i class="icon-bubbles4"></i> <span>الرسائل</span></a>
                 <ul class="nav nav-group-sub" data-submenu-title="الرسائل">
                     <li class="nav-item"><a href="{{route('mail.index')}}" class="nav-link {{active()->route('mail.index')}}">جميع الرسائل</a></li>
@@ -108,7 +124,11 @@
                 </ul>
             </li>
 
-            <li class="nav-item nav-item-submenu">
+
+            <li class="nav-item-header"><div class="text-uppercase font-size-xs line-height-xs">قائمة الأعدادات</div> <i class="icon-menu" title="قائمة الأعدادات"></i></li>
+
+
+            <li class="nav-item nav-item-submenu @if(active()->route('about_us.*')) nav-item-expanded nav-item-open @else  @endif">
                 <a href="{{route('about_us.index')}}" class="nav-link {{active()->route('about_us.*')}}"><i class="icon-info3"></i> <span>حول الموقع</span></a>
                 <ul class="nav nav-group-sub" data-submenu-title="حول الموقع">
                      @if(count($aboutUs) == 0)
@@ -119,7 +139,8 @@
                 </ul>
             </li>
 
-            <li class="nav-item nav-item-submenu">
+
+            <li class="nav-item nav-item-submenu @if(active()->route('settings.*')) nav-item-expanded nav-item-open @else  @endif">
                 <a href="{{route('settings.index')}}" class="nav-link {{active()->route('settings.*')}}"><i class="icon-gear"></i> <span>الإعدادات</span></a>
                 <ul class="nav nav-group-sub" data-submenu-title="الإعدادات">
                     @if(count($settings) == 0)
