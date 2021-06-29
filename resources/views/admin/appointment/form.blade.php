a2a2f6b8f6a3d8b052c44ca75ae1e5d619b0de44
baraaofficial <baraa_official@outlook.com>
2021-06-29 19:19:15 +0200

    update

--- resources/views/admin/appointment/form.blade.php
+++ resources/views/admin/appointment/form.blade.php
@@ -31,7 +31,7 @@
     <label class="col-form-label col-lg-2">الحالة<span class="text-danger">*</span></label>
     <div class="col-lg-6">
         <select name="{{$status}}" class="form-control select-search" data-fouc data-placeholder="اختار الحاله" required>
-            <option></option>
+
             <option value="1" {{ isset($model) && $model->{$status} == '1' ? 'selected'  : '' }}>نشط</option>
             <option value="0" {{ isset($model) && $model->{$status} == '0' ? 'selected'  : '' }}>معلق</option>
         </select>
